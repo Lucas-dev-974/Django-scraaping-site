550ec4891d3d95b48a9c1f616a439b3d4a3d6a04
lcs lvn <lucas.lvn97439@gmail.com>
2022-09-21 09:17:30 +0200

    code reviews

--- scrapping_site/urls.py
+++ scrapping_site/urls.py
@@ -1,8 +1,7 @@
-from xml.etree.ElementInclude import include
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('TodoList.urls')),
+    path('', include('Site.urls')),
 ]
