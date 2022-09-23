73e17bda526a66ecdc8dad812bbc7d022713b1bc
lcs lvn <lucas.lvn97439@gmail.com>
2022-09-23 05:53:33 +0200

    append of private home page and setting up redirect url if user is not connected

--- Site/views/TargetSite.py
+++ Site/views/TargetSite.py
@@ -1,15 +1,20 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-@login_required
+@login_required(login_url=('/login'))
+def Private(request):
+    return render(request, 'template-parts/TargetSite.html')
+
+
+@login_required(login_url=('/login'))
 def TGSite(request):
     print('okok')
     return render(request, 'template-parts/TargetSite.html')
 
-@login_required
+@login_required(login_url=('/login'))
 def TGS_History(request):
     return render(request, 'template-parts/history.html')
 
-@login_required 
+@login_required(login_url=('/login'))
 def TGS_Graph(request):
     return render(request, 'template-parts/graph-charts.html')
