346ab70d66031db544d577ba51337ecb62633adc
lucas-lvn <lucas.lvn9439@gmail.com>
2022-09-26 14:57:58 +0400

    append of url for private page

--- Site/urls.py
+++ Site/urls.py
@@ -6,8 +6,10 @@ urlpatterns = [
     path('',         Home,   name = 'home'),
     path('login',    Login,  name='login'),
     path('register', Register, name='register'),
-    path('tg-site/',  include([
-        path('',         TGSite, name='target-site'),
+    path('logout',    Logout,  name='logout'),
+    path('private',  include([
+        path('',    private, name='private'), 
+        path('tg-site',  TGSite, name='target-site'),
         path('history',  TGS_History),
         path('graph',    TGS_Graph) 
     ])),
