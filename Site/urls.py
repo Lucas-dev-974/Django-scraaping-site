981360e8e9931e6a7b894e6a92841dcc5b3bfc15
lucas-lvn <lucas.lvn9439@gmail.com>
2022-09-28 14:40:42 +0400

    URL - append of Graph api url

--- Site/urls.py
+++ Site/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
         path('tg-site/<int:id>',  TGSite, name='target-site'),
         path('history',  TGS_History),
         path('graph',    TGS_Graph), 
+        path('graph/api',    TGS_graph_api), 
         path('relevee/', include([
             path('<int:siteid>',    Releve, name='relevee'), 
             path('scrap/start/<int:siteid>', Releve)
