fc87d8253f3228ea48f27c600fd8f3c07195505a
lucas-lvn <lucas.lvn9439@gmail.com>
2022-09-30 07:41:44 +0400

    code review

--- static/js/graphThr.js
+++ static/js/graphThr.js
@@ -27,45 +27,41 @@ async function getGraphData(url=null, method='POST', params=null, csrfToken=fals
     return JsonResponse
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    getGraphData('http://127.0.0.1:8000/private/graph/api', method='POST', params={
+document.addEventListener('DOMContentLoaded', async () => {
+    const response = await getGraphData('http://127.0.0.1:8000/private/graph/api', method='POST', params={
         'year': '2022',
         'page': '0'
     }, '{{ csrf_token }}')
+    
+    console.log(response);
+    const myChart = new Chart(document.getElementById('myChart'),config);
 })
 
-const labels = [
-    'Janvier',
-    'Fevrier',
-    'Mars',
-    'Avril',
-    'Mai',
-    'Juin',
-  ];
-
 const data = {
-labels: labels,
-datasets: [{
-    type: 'bar',
-    label: 'Threads Scrapped',
-    backgroundColor: 'rgba(99, 198, 255, 0.2)',
-    borderColor: 'rgba(99, 190, 255,1)',
-    borderWidth: 2,
-    hoverBackgroundColor: "rgba(99, 167, 255, 0.4)",
-    hoverBorderColor: "rgba(78, 124, 199, 1)",
-    data: [1, 10, 5, 2, 20, 15, 25],
-},
-{
-  type: 'line',
-  label: 'Threads (moy) Scrapped',
-  backgroundColor: 'rgba(55, 146, 66, 1)',
-  borderColor: 'rgba(55, 146, 66, 0.7)',
-  data: [1, 5, 2, 1, 10, 8, 0],
-}]
+  labels: labels,
+  datasets: [
+    {
+        type: 'bar',
+        label: 'Threads Scrapped',
+        backgroundColor: 'rgba(99, 198, 255, 0.2)',
+        borderColor: 'rgba(99, 190, 255,1)',
+        borderWidth: 2,
+        hoverBackgroundColor: "rgba(99, 167, 255, 0.4)",
+        hoverBorderColor: "rgba(78, 124, 199, 1)",
+        data: [1, 10, 5, 2, 20, 15, 25],
+    },
+    {
+      type: 'line',
+      label: 'Threads (moy) Scrapped',
+      backgroundColor: 'rgba(55, 146, 66, 1)',
+      borderColor: 'rgba(55, 146, 66, 0.7)',
+      data: [1, 5, 2, 1, 10, 8, 0],
+    }
+  ]
 };
 
 const config = {
-data: data,
+  data: data,
   options: {
     scales: {
       y: {
@@ -82,4 +78,3 @@ data: data,
   }
 };
 
-const myChart = new Chart(document.getElementById('myChart'),config);
