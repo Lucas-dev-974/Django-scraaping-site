5fe737de4c93d913e5da6b688991c4eb07cac9af
lucas-lvn <lucas.lvn9439@gmail.com>
2022-10-05 13:30:04 +0400

    SQLite3 to MySQL

--- scrapping_site/settings.py
+++ scrapping_site/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'scrapping_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'oho_scrapping',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
